7c42531d1dbdeea59fe97b5b235a00f69fb27848
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2016-02-10 18:22:29 -0500

    specify the git repos needed

--- math-lib/info.rkt
+++ math-lib/info.rkt
@@ -4,8 +4,8 @@
 
 (define deps '("base"
                "r6rs-lib"
-               "typed-racket-lib"
-               "typed-racket-more"
+               "git://github.com/andmkent/typed-racket?path=typed-racket-lib#rtr-prototype"
+               "git://github.com/andmkent/typed-racket?path=typed-racket-more#rtr-prototype"
                ("math-i386-macosx" #:platform "i386-macosx")
                ("math-x86_64-macosx" #:platform "x86_64-macosx")
                ("math-ppc-macosx" #:platform "ppc-macosx")
